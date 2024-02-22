91807464d2f3f450a6395c73f499b3b0493b4637
HemanjaliK <157987649+HemanjaliK@users.noreply.github.com>
2024-02-22 03:20:31 -0500

    Delete app/commands directory

--- app/commands/cool/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.commands import Command
-
-
-class coolCommand(Command):
-    def execute(self):
-        print("This calculator project is so cool!")
--- app/commands/thanks/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.commands import Command
-
-
-class thanksCommand(Command):
-    def execute(self):
-        print("Thank You!")
